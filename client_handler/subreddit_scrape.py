93a85200bb4a06a90fba04f67a44cdaaa1942d6a
ejbills <74191134+ejbills@users.noreply.github.com>
2024-01-28 18:07:45 -0800

    Keyword searching (#6)
    * update package dependencies
    
    * add support for search queries

--- client_handler/subreddit_scrape.py
+++ client_handler/subreddit_scrape.py
@@ -14,18 +14,25 @@ reddit = praw.Reddit(
 )
 
 
-def scrape_top_posts(daily_check, filter_dict, subreddit) -> list:
+def scrape_top_posts(daily_check, filter_dict, query) -> list:
     # Execute subreddit praw call with filter logic
-    subreddit_stream = []
     parsed_submissions = []
     temp_submission_stream = []  # Handle saving subreddit stream to prevent duplicate praw calls
 
+    is_search = filter_dict['is_search'] if 'is_search' in filter_dict else False
+    subreddit_scope = query if not is_search else 'all'  # determine scope for query, if it's a search -> search all
+
+    time_filter = 'day' if daily_check else 'week'
+
     # Check if subreddit previously scraped
-    if subreddit not in conf.config.scraped_subreddits.keys():
-        subreddit_stream = reddit.subreddit(subreddit).top('day' if daily_check else 'week')
+    if query not in conf.config.prev_searched_queries.keys():
+        if is_search:
+            subreddit_stream = reddit.subreddit(subreddit_scope).top(time_filter)
+        else:
+            subreddit_stream = reddit.subreddit(subreddit_scope).search(query, sort='top', time_filter=time_filter)
 
     else:
-        subreddit_stream = conf.config.scraped_subreddits[subreddit]
+        subreddit_stream = conf.config.prev_searched_queries[query]
 
     for submission in subreddit_stream:
         temp_submission_stream.append(submission)  # Save submissions to prevent repeating search
@@ -40,17 +47,23 @@ def scrape_top_posts(daily_check, filter_dict, subreddit) -> list:
                 parsed_submissions.append([submission.title, submission.url,
                                            reddit.config.reddit_url + submission.permalink])
 
-    conf.config.scraped_subreddits[subreddit] = temp_submission_stream
+    conf.config.prev_searched_queries[query] = temp_submission_stream
 
     return parsed_submissions
 
 
 def contains_flairs(filter_dict, submission) -> bool:
     # Checks if the post flair passes validation based on dictionary flair values
-    if all(len(flair_prefs) > 0 for flair_prefs in filter_dict.values()):
+    stripped_filter_dict = filter_dict
+
+    if 'is_search' in filter_dict: stripped_filter_dict.pop('is_search')  # we can ignore the search element
+
+    if all(len(flair_prefs) > 0 for flair_prefs in stripped_filter_dict.values()):
         if submission.link_flair_text is not None:
-            required_flairs = any(flair in submission.link_flair_text.lower() for flair in filter_dict['required_flairs'])
-            restricted_flairs = not any(flair in submission.link_flair_text.lower() for flair in filter_dict['restricted_flairs'])
+            required_flairs = any(
+                flair in submission.link_flair_text.lower() for flair in filter_dict['required_flairs'])
+            restricted_flairs = not any(
+                flair in submission.link_flair_text.lower() for flair in filter_dict['restricted_flairs'])
 
             return required_flairs and restricted_flairs  # Everything passed, post is valid
 
@@ -66,5 +79,5 @@ def is_stickied(submission) -> bool:
 
 
 def is_urgent(submission) -> bool:
-    # Checks if a post is considered as "urgent" - this is defined as over 200 upvotes on the post in a given day
+    # Checks if a post is considered as "urgent" - this is defined as over 200 up-votes on the post in a given day
     return submission.score > 200
